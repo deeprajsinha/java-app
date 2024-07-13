28cc3290ade75a04161efef91ac32a75a9eec31b
deepak.kumar <Deepak.kumar@mpokket.com>
2024-07-13 22:10:36 +0530

    initial

---
+++ src/main/java/in/mpokket/java_test_servie/JavaTestServieApplication.java
@@ -0,0 +1,13 @@
+package in.mpokket.java_test_servie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestServieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestServieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/mpokket/java_test_servie/JavaTestServieApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mpokket.java_test_servie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestServieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
